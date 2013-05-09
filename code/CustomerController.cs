5a394440235cd1d9fa9c7027c96e83a697699261
Josh Sullivan <jjs5286@gmail.com>
2013-05-08 22:15:41 -0400

    I did it!

---
+++ code/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SullysStraps.Data.IRepos;
+using SullysStraps.Model;
+
+namespace SullysStraps.Web.Controllers
+{
+    public class CustomerController : ApiControllerBase
+    {
+        public CustomerController(ISullysStrapsUow uow)
+        {
+            Uow = uow;
+        }
+        // GET api/customer
+        public IEnumerable<Customer> Get()
+        {
+            return Uow.Customers.GetAll().OrderBy(c => c.LName);
+        }
+
+        // GET api/customer/5
+        public Customer Get(int id)
+        {
+            var customer = Uow.Customers.GetById(id);
+            if (customer != null) return customer;
+            throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+        }
+
+        // POST api/customer
+        public HttpResponseMessage Post(Customer customer)
+        {
+            Uow.Customers.Add(customer);
+            Uow.Commit();
+            var response = Request.CreateResponse(HttpStatusCode.Created, customer);
+            response.Headers.Location = new Uri(Url.Link(RouteConfig.ControllerAndId, new { id = customer.CustomerId }));
+            return response;
+        }
+
+        // PUT api/customer
+        public HttpResponseMessage Put(Customer customer)
+        {
+            Uow.Customers.Update(customer);
+            Uow.Commit();
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+
+        // DELETE api/customer/5
+        public HttpResponseMessage Delete(int id)
+        {
+            Uow.Customers.Delete(id);
+            Uow.Commit();
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ dotfiles/JOSHREADME.txt
@@ -0,0 +1,14 @@
+The 3 Essential add-on's I'm using right now for VS
+
+Resharper - It cleans your code, it tells you better ways to do things and has all sorts of helpful features. The "Goto Implementation" feature
+	    justifies the cost of this tool alone.
+
+Web Essentials - Free add-on that basically just makes writing CSS and HTML a little easier
+
+Visual Studio Achievments by Channel9 - Because nothing is better than opening up the war-torn remains of a client's code and seeing an 
+	achievement pop up in the bottom right corner of VS that says "Job Security - Write 20 single letter class level variables in one file. Kudos to you for being cryptic!"
+
+
+
+
+We only used basic Eclipse, PHPMyAdmin, and Putty when I was working with PHP at Blistt so I can't really give an opinion on it. 
