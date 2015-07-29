d6992b0dd657df6c3d7fb690dfabc4de053e5563
Adam McMahon <adam@Adams-MacBook-Pro.local>
2015-07-28 20:10:00 -0400

    dispensary finder initial commit

---
+++ code/index.php
@@ -0,0 +1,195 @@
+<?php
+
+class Colorado {
+	var $droughts = true; //:(
+	var $forestFires = true; //:(
+	var $longerRidingSeason = true; //:)
+	var $sparkfun = true; //:)
+	var $dancing = true; //:)
+}
+
+class DispensaryFinder EXTENDS Colorado {
+	
+	private $dispensaries = array('Solace','Sparkfun','Native Roots');
+	private $hasMedicalCard = null;
+	private $is21orOlder = null;
+	private $apiUrl = '';
+	var $simulateLoading = null;
+
+	function __construct($simulateLoading = true)
+	{
+		$this->simulateLoading = true;
+		$msg = "Welcome to Colorado\n";
+		$msg .= "'Come for the weed! Stay for the weed!'\n";
+		$msg .= '_\|/_ _\|/_ _\|/_ _\|/_ _\|/_ _\|/_ _\|/_';
+		$msg .= "\n\n";
+		echo $msg;
+		$this->prompt();
+		$this->fetchDispensaries();
+		$this->recommend();
+
+	}
+
+	/**
+	 * Prompt user to ensure requirements are met
+	 *
+	 * @return void
+	 **/
+
+
+	private function prompt()
+	{
+		echo 'Do you have a medical card? [Y/N]:';
+		//if y skip next 1
+		if(!$this->hasMedicalCard = $this->answer()) {
+			echo 'Are you 21 or older? [Y/N]:';		
+			if(!$this->is21orOlder = $this->answer()) {
+				echo 'Do you have a sibling or cousin who is 21 or older? [Y/N]:';
+				if($this->answer()) {
+					echo 'Please sit him/her in front of the screen then presss enter to continue:';
+					$this->answer();
+				} else {
+					die("It's black market for you buddy. Sorry :(\n");
+				}
+			}
+		}
+	}
+
+	/**
+	 * Simulate loading. Travel websites do this sh*t on purpose too "The more you know!"
+	 *
+	 * @param $seconds (int) how many seconds do you want to delay?
+	 * @param $loader (string) loading text to repeat
+	 * @return void
+	 **/
+
+	public static function simulateLoading($seconds = 3,$loader = '.') {
+		for($i = 0;$i < $seconds;$i++)
+		{
+			sleep(1);
+			echo $loader;
+		}
+	}
+
+	/**
+	 * Check prompt answer
+	 * 
+	 * @return void
+	 **/
+
+	private function answer()
+	{
+		$handle = fopen ("php://stdin","r");
+		$line = fgets($handle);
+		fclose($handle);
+		if(strtolower(trim($line)) != 'y'){
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Recommends dispensary from list
+	 *
+	 * @return void
+	 **/
+
+	private function recommend()
+	{
+		if(empty($this->dispensaries)){
+			die("Uh OH! Out of dispensaries. Better head to Washington!\n");
+		}
+
+		$dispensary = array_pop($this->dispensaries);
+		$msg = 'How about this dispensary? '."\n\n";
+		$msg.= $dispensary."\n\n[Y/N]:";
+		echo $msg;
+		if($this->answer()){
+			$this->medPsa();
+		} else {
+			// get a load of that recursion
+			$this->recommend();
+		}
+
+	}
+
+	/**
+	 * A friendly PSA
+	 *
+	 * @return void
+	 **/
+
+	 public static function medPsa()
+	 {
+	 	$delay = 2;
+
+		echo "Remember GANJA and wheels don't mix.\n";
+		echo "Don't get high while being high and in drive!\n";
+		self::simulateLoading($delay,'');
+		echo "Or in gear!\n";
+		self::simulateLoading($delay,'');
+		echo "Or in neutral!\n";
+		self::simulateLoading($delay,'');
+		echo "Or in park!\n";
+		self::simulateLoading($delay,'');
+		echo "Oh just go home and watch Cartoon Network!\n";
+		self::simulateLoading($delay,'');
+		echo "NOW!\n";
+		self::simulateLoading($delay,'');
+		echo "SERIOUSLY!\n";
+		self::simulateLoading($delay,'');
+		echo "Message brought to you by the Colorado Marijuana Enforcement Division.\n";
+		self::simulateLoading($delay,'');
+		echo "Serious bidness YO!\n";
+		self::simulateLoading($delay,'');
+		echo "k thanx bai now\n";
+		self::simulateLoading(1,'');
+		//could of put this in an array but yeah next version
+	 }
+
+	/**
+	 * Retrieves list of dispensaries from flat file
+	 *
+	 * @return void
+	 **/
+
+	private function fetchDispensaries()
+	{
+		echo 'Fetching Dispensaries...';
+		if($this->simulateLoading) $this->simulateLoading();
+		echo "\n";
+		if($this->hasMedicalCard()){
+
+		} else {
+
+		}
+
+		/*$file = file_get_contents('');
+		$this->dispensaries = json_decode($file);*/
+
+	}
+
+	/**
+	 * Does the user have a medical card?
+	 *
+	 * @return boolean
+	 **/
+
+	public function hasMedicalCard()
+	{
+		return $this->hasMedicalCard;
+	}
+
+	/**
+	 * Is the user 21 or older?
+	 * 
+	 * @return boolean
+	 **/
+
+	public function is21orOlder()
+	{
+		return $this->is21orOlder;
+	}
+}
+
+new DispensaryFinder();
